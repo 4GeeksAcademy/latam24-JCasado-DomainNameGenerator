143ffe9d6fcabc215fee7582c2b6d98e120780af
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2023-11-14 02:18:29 +0000

    Second commit

--- src/app.js
+++ src/app.js
@@ -5,22 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function() 
+{
   const pronouns = ["the","our"];
+
   const adjs = ["great","big"];
+
   const nouns = ["jogger","racoon"];
+
   const extens = [".com",".us",".io",".es"];
 
-  for (let pronoun of pronouns) {
-    for (let adj of adjs) {
-      for (let noun of nouns) {
-        for (let exten of extens) {
+  for (let pronoun of pronouns) 
+  {
+    for (let adj of adjs) 
+    {
+      for (let noun of nouns) 
+      {
+        for (let exten of extens) 
+        {
           console.log(pronoun,adj,noun,exten);
         }
       }
     }
   }
 };
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+console.log("Hello Rigo from the console!");
