f8fadf7f8c20dba9dd2ae27d6ca08dd0c70552d5
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2023-11-14 02:13:49 +0000

    Ready for testing.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  const pronouns = ["the","our"];
+  const adjs = ["great","big"];
+  const nouns = ["jogger","racoon"];
+  const extens = [".com",".us",".io",".es"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let exten of extens) {
+          console.log(pronoun,adj,noun,exten);
+        }
+      }
+    }
+  }
+};
   //write your code here
   console.log("Hello Rigo from the console!");
 };
