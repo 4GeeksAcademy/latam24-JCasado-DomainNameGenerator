d1f6d229c9d7deb30527391c2f1195b0d5f65930
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2023-11-16 00:28:03 +0000

    Ready to be approved :)

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
-window.onload = function() {
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() 
+{
   const pronouns = ["the", "our"];
 
   const adjectives = ["great", "big"];
@@ -7,11 +15,15 @@ window.onload = function() {
 
   const extens = [".com", ".us", ".io", ".es"];
 
-  for (let pronoun of pronouns) {
-    for (let adj of adjectives) {
-      for (let noun of nouns) {
-        for (let exten of extens) {
-          console.log(pronoun + adj + noun + exten);
+  for (let pronoun of pronouns) 
+  {
+    for (let adj of adjectives) 
+    {
+      for (let noun of nouns) 
+      {
+        for (let exten of extens) 
+        {
+          console.log (pronoun + adj + noun + exten);
         }
       }
     }
